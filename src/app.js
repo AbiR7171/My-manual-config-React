aeed50bbd8eeca3b83c143379eaec7471ca3ad90
AbiR7171 <abiremtiag@gmail.com>
2024-01-11 01:29:37 +0600

    Config finish

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HelloWorld from "./helloword";
+
+
+ const App = () => {
+         return (
+             <div>
+                 <p>AbiR Beautiful</p>
+                   <HelloWorld/>
+             </div>
+         )
+}
+
+
+export default App
